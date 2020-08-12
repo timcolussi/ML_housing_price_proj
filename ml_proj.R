a6437e0ca92a5f6da66c4d97aa211dbfa1774906
Tim Colussi <tim.colussi@gmail.com>
2020-08-11 21:52:00 -0400

    changes

--- ml_proj.R
+++ ml_proj.R
@@ -138,21 +138,151 @@ glimpse(modeling_data)
 modeling_data[] <- lapply(modeling_data, function(x) if(is.character(x)) as.factor(x) else x)
 modeling_data$MSSubClass <- as.factor(modeling_data$MSSubClass)
 
-library(lubridate)
-year(modeling_data$YearBuilt)
+modeling_data$TotalBsmtSF <- NULL
+modeling_data$GrLivArea <- NULL
+
+# library(lubridate)
+# 
+# year_blt <- strptime(modeling_data$YearBuilt, "%Y")
+# mo_blt <- strptime(modeling_data$MoSold, "%M")
+# year(year_blt)
+# month(mo_blt)
+# 
+# head(modeling_data$MoSold, 15)
+
+# drop Utilities because they are all the same
+modeling_data$Utilities <- NULL
 
 set.seed(2187)
 index <- sample(1:nrow(modeling_data), size=nrow(modeling_data)*0.8)
 train <- modeling_data[index, ]
 test <- modeling_data[-index, ]
 
-
-
 mlr_model <- lm(LogSalePrice ~ ., train)
 
+summary(mlr_model)
+library(car)
+vif(mlr_model)
+
+ld.vars <- attributes(alias(mlr_model)$Complete)$dimnames[[1]]
+ld.vars
+
+mlr.empty <- lm(LogSalePrice ~ 1, data = train)
+mlr.saturated <- lm(LogSalePrice ~ ., data = train)
+scope = list(lower = formula(mlr.empty), upper = formula(mlr.saturated))
+
+library(MASS)
+backwardAIC = step(mlr.saturated, scope, direction = "backward", k = 2)
+forwardAIC = step(mlr.empty, scope, direction = "forward", k = 2)
+
+summary(backwardAIC)
+summary(forwardAIC)
+vif(backwardAIC)
+vif(forwardAIC)
+
+modeling_data2 <- modeling_data
+modeling_data2$LogSalePrice <- NULL
+cor(modeling_data)
+
+
+##
+
+# formula.new <- as.formula(
+#   paste(
+#     paste(deparse(formula), collapse=""), 
+#     paste(ld.vars, collapse="-"),
+#     sep="-"
+#   )
+# )
+# 
+# 
+# fit.new <-lm(formula.new)
+# vif(fit.new)
 
 
 
 
 
 
+
+
+# fixing contraat error
+# glimpse(train)
+# 
+# debug_contr_error <- function (dat, subset_vec = NULL) {
+#   if (!is.null(subset_vec)) {
+#     ## step 0
+#     if (mode(subset_vec) == "logical") {
+#       if (length(subset_vec) != nrow(dat)) {
+#         stop("'logical' `subset_vec` provided but length does not match `nrow(dat)`")
+#       }
+#       subset_log_vec <- subset_vec
+#     } else if (mode(subset_vec) == "numeric") {
+#       ## check range
+#       ran <- range(subset_vec)
+#       if (ran[1] < 1 || ran[2] > nrow(dat)) {
+#         stop("'numeric' `subset_vec` provided but values are out of bound")
+#       } else {
+#         subset_log_vec <- logical(nrow(dat))
+#         subset_log_vec[as.integer(subset_vec)] <- TRUE
+#       } 
+#     } else {
+#       stop("`subset_vec` must be either 'logical' or 'numeric'")
+#     }
+#     dat <- base::subset(dat, subset = subset_log_vec)
+#   } else {
+#     ## step 1
+#     dat <- stats::na.omit(dat)
+#   }
+#   if (nrow(dat) == 0L) warning("no complete cases")
+#   ## step 2
+#   var_mode <- sapply(dat, mode)
+#   if (any(var_mode %in% c("complex", "raw"))) stop("complex or raw not allowed!")
+#   var_class <- sapply(dat, class)
+#   if (any(var_mode[var_class == "AsIs"] %in% c("logical", "character"))) {
+#     stop("matrix variables with 'AsIs' class must be 'numeric'")
+#   }
+#   ind1 <- which(var_mode %in% c("logical", "character"))
+#   dat[ind1] <- lapply(dat[ind1], as.factor)
+#   ## step 3
+#   fctr <- which(sapply(dat, is.factor))
+#   if (length(fctr) == 0L) warning("no factor variables to summary")
+#   ind2 <- if (length(ind1) > 0L) fctr[-ind1] else fctr
+#   dat[ind2] <- lapply(dat[ind2], base::droplevels.factor)
+#   ## step 4
+#   lev <- lapply(dat[fctr], base::levels.default)
+#   nl <- lengths(lev)
+#   ## return
+#   list(nlevels = nl, levels = lev)
+# }
+# debug_contr_error2 <- function (form, dat, subset_vec = NULL) {
+#   ## step 0
+#   if (!is.null(subset_vec)) {
+#     if (mode(subset_vec) == "logical") {
+#       if (length(subset_vec) != nrow(dat)) {
+#         stop("'logical' `subset_vec` provided but length does not match `nrow(dat)`")
+#       }
+#       subset_log_vec <- subset_vec
+#     } else if (mode(subset_vec) == "numeric") {
+#       ## check range
+#       ran <- range(subset_vec)
+#       if (ran[1] < 1 || ran[2] > nrow(dat)) {
+#         stop("'numeric' `subset_vec` provided but values are out of bound")
+#       } else {
+#         subset_log_vec <- logical(nrow(dat))
+#         subset_log_vec[as.integer(subset_vec)] <- TRUE
+#       } 
+#     } else {
+#       stop("`subset_vec` must be either 'logical' or 'numeric'")
+#     }
+#     dat <- base::subset(dat, subset = subset_log_vec)
+#   }
+#   ## step 0 and 1
+#   dat_internal <- stats::lm(form, data = dat, method = "model.frame")
+#   attr(dat_internal, "terms") <- NULL
+#   ## rely on `debug_contr_error` for steps 2 to 4
+#   c(list(mf = dat_internal), debug_contr_error(dat_internal, NULL))
+# }
+# debug_contr_error2(LogSalePrice ~ ., train)
+# 
+# table(housing_data_raw$Utilities)
